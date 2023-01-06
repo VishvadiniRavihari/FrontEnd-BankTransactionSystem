ca6754aa4ee90aa58264d414a44cfed37734db19
VishvadiniRK <vishvadinirk4@gmail.com>
2023-01-06 09:27:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CustomerReg from './forms/CustomerRegistration';
+import {BrowserRouter, Route,Routes} from 'react-router-dom';
+import EmployeeHome from './pages/EmployeePortal/EmployeeHome';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1>hello</h1>
+    <BrowserRouter>
+        <Routes>
+          <Route path = '/EmployeePortal'>
+            <Route exact path = '/EmployeePortal' element = {<EmployeeHome/>}/>
+              <Route path = 'customer-register' element = {<CustomerReg/>} />
+          </Route>
+          
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/EmployeePortal/EmployeeHome.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import CustomerHandling from './CustomerHandling'
+export default function EmployeeHome(){
+    return (
+        <div>
+            <CustomerHandling/>
+        </div>
+    )
+}
